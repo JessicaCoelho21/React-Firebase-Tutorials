b873a8ab93f1a322d964c9482e4aa05a7a96518a
JessicaCoelho21 <jscoelho21@gmail.com>
2021-05-03 23:32:08 +0100

    Tutorial de autenticação -> Sign Up

---
+++ react-firebase-authentication/src/Components/SignUp.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Card, Form, Button } from "react-bootstrap";
+
+export default function SignUp() {
+  const emailRef = React.useRef();
+  const passwordRef = React.useRef();
+  const passwordConfirmRef = React.useRef();
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          <Form>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+
+            <Form.Group id="password-confirm">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+
+            <Button className="w-100" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+
+      <div className="w-100 text-center mt-2">
+        Already have an account? Login (futuramente será um link)
+      </div>
+    </>
+  );
+}
