281bf7d4d682f8eac4f2337cd261e6dd312a0ba7
JessicaCoelho21 <jscoelho21@gmail.com>
2021-05-04 22:57:09 +0100

    Authentication Tutorial -> Dashboard/Profile

--- react-firebase-authentication/src/Components/Dashboard.js
+++ react-firebase-authentication/src/Components/Dashboard.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { Button, Card, Alert } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuth } from "../Contexts/AuthContext";
 
 export default function Dashboard() {
+  const [error, setError] = React.useState("");
+  const { currentUser } = useAuth();
+
+  function handleLogout() {}
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email: </strong> {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Logout
+        </Button>
+      </div>
+    </>
   );
 }
