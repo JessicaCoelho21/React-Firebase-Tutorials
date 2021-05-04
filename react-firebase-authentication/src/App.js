7642e31b47c311cfa926873e35e8432c49b32e33
JessicaCoelho21 <jscoelho21@gmail.com>
2021-05-04 23:31:26 +0100

    Authentication Tutorial -> Private Routes

--- react-firebase-authentication/src/App.js
+++ react-firebase-authentication/src/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from "./Contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard";
 import Login from "./Components/Login";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Router>
           <AuthProvider>
             <Switch>
-              <Route exact path="/" component={Dashboard} />
+              <PrivateRoute exact path="/" component={Dashboard} />
               <Route path="/signup" component={SignUp} />
               <Route path="/login" component={Login} />
             </Switch>
